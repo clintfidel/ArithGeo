f6a6277225757d0a5477a95a9bcc2e5734326f18
clintfidel <clintfidel@gmail.com>
2017-08-28 13:28:56 +0100

    add functions for Arithmetic,Geometric numbers and niether of them

---
+++ aritGeo/app.js
@@ -0,0 +1,37 @@
+const AritGeo =
+    aritGeo = (arr) => {
+    if(Array.isArray(arr)) {
+      let arith = true;
+      let geo = true;
+      let checkArit = arr[1]-arr[0];
+      let checkGeo = arr[1]/arr[0];
+      if(arr.length > 0) {
+        for (let i = 0; i<arr.length-1; i++) {
+          if (arr[i+1] - arr[1] !== checkArit){
+             arit = false;
+          }
+          
+          if (arr[i+1]/checkGeo !== arr[i]) {
+            geo = false;
+          }
+        }
+        if (arith === true){
+          return "Arithmetic";
+        }
+        else if (geo ===true) {
+          return "Geometric";
+        }
+        else {
+          return -1;
+        }  
+      }
+      else {
+        return 0;
+      }
+      
+    }
+    else {
+      return "I can't be empty; Pls pass a valid array";
+    }
+  }
+module.exports = AritGeo;
